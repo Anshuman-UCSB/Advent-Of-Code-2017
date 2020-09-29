b085588efe7587723f61ab62685cf37999e01baf
Anshuman-UCSB <anshuman@ucsb.edu>
2020-09-29 14:42:04 -0700

    FURTHER THAN EVER BEFORE

--- Day 10/main.cpp
+++ Day 10/main.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <iostream>
 #include <vector>
+#include <iomanip>
 #include <stack>
 using namespace std;
 
@@ -25,34 +26,51 @@ int main(){
     }
     vector<int> extra = {17, 31, 73, 47, 23};
     inp.insert(inp.end(), extra.begin(), extra.end());
-    for(auto& i: inp){
-        cout<<i<<endl;
-    }
-    exit(1);
+    // for(auto& i: inp){
+    //     cout<<i<<endl;
+    // }
+    
     vector<int> v;
     int SIZE = 256;
     
     for(int i = 0;i<SIZE;i++)
         v.push_back(i);
 
-    for(auto val: v)
-        cout<<val<<endl;
+    // for(auto val: v)
+    //     cout<<val<<endl;
 
     int ind = 0;
     int skip = 0;
     stack<int> s;
-    for(int len:inp){
-        for(int i = 0; i<len;i++){
-            s.push(v[(ind+i)%v.size()]);
+    for(int i = 0;i<64;i++){
+        for(int len:inp){
+            for(int i = 0; i<len;i++){
+                s.push(v[(ind+i)%v.size()]);
+            }
+            for(int i = 0; i<len;i++){
+                v[(ind+i)%v.size()] = s.top();
+                s.pop();
+            }
+            // for(auto val: v)
+            //     cout<<val<<" ";
+            // cout<<endl;
+            ind+=len+skip++;
         }
-        for(int i = 0; i<len;i++){
-            v[(ind+i)%v.size()] = s.top();
-            s.pop();
-        }
-        for(auto val: v)
-            cout<<val<<" ";
-        cout<<endl;
-        ind+=len+skip++;
     }
+    vector<int> sparse;
+    ind = 0;
+    for(int i = 0;i<16;i++){
+        int val = 0;
+        for(int j =0;j<16;j++)
+            val ^= v[ind++];
+        sparse.push_back(val);
+    }
+
+    stringstream hx;
+    for(auto val: sparse)
+        hx<<setw(2)<<setfill('0')<<hex<<val;
+
+
     cout<<"Part 1: "<<v[0]*v[1]<<endl;
+    cout<<"Part 2: "<<hx.str()<<endl;
 }
