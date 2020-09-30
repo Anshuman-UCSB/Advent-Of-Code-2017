b34f163dafd3db9b0bb99933f050d9287835d2c5
Anshuman-UCSB <anshuman@ucsb.edu>
2020-09-29 20:54:06 -0700

    Utility

---
+++ utility.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <sstream>
+using namespace std;
+
+template<typename T>
+ostream& operator<< (ostream& out, const vector<T>& v) {
+    out << "{";
+    size_t last = v.size() - 1;
+    for(size_t i = 0; i < v.size(); ++i) {
+        out << v[i];
+        if (i != last) 
+            out << ", ";
+    }
+    out << "}";
+    return out;
+}
