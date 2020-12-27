463d91c471f468d9cca7f2e0e144f8366c8d1370
Anshuman-UCSB <anshuman@ucsb.edu>
2020-12-26 20:33:05 -0800

    Day 10 done

--- Day 10/main.jl
+++ Day 10/main.jl
@@ -1,15 +1,50 @@
-
-
-let lens = "input"|>open|>readline|>x->split(x,",")|>x->parse.(Int, x), regs = [i for i in 0:4]
-    curr, skip = 1, 0
-    for l in lens
-        println("reg : ",regs)
-        regs = circshift(regs, -curr)
-        regs = reverse(regs, 1, l);
-        regs = circshift(regs, curr)
-        skip+=1
-        curr+=skip
-        mod1(curr, length(regs))
-    end
-    println("reg : ",regs)
+input = map(Int, "input"|>open|>readline|>collect)
+append!(input, [17, 31, 73, 47, 23])
+regs = [x for x in 0:255]
+function rev(regs, start, size)
+    temp = copy(regs)
+    circshift!(temp, regs, -start)
+    reverse!(temp, 1, size)
+    circshift!(regs, temp, start)
 end
+
+function knotHash(input)
+    input = map(Int,collect(input))
+    append!(input, [17, 31, 73, 47, 23])
+    let i=0, skip = 0, hash = ""
+        for _ in 1:64
+            for l in input
+                rev(regs, i, l)
+                i+=l+skip
+                i%=length(regs)
+                skip+=1
+            end
+        end
+        for i in 1:16:255
+            dense = string(reduce(xor,regs[i:i+15]), base=16)
+            dense = length(dense)>1 ? dense : "0"*dense
+            hash = hash*dense
+        end
+        return hash
+    end
+end
+
+knotHash("")
+
+# let i=0, skip = 0, hash = ""
+#     for _ in 1:64
+#         for l in input
+#             rev(regs, i, l)
+#             i+=l+skip
+#             i%=length(regs)
+#             skip+=1
+#         end
+#     end
+#     for i in 1:16:255
+#         dense = string(reduce(xor,regs[i:i+15]), base=16)
+#         dense = length(dense)>1 ? dense : "0"*dense
+#         hash = hash*dense
+#     end
+#     hash|>println
+# end
+
